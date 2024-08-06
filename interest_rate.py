84069413d5d224fc05731db5fb1df58822a6cf2a
smpagola <pagolincom@gmail.com>
2024-08-06 13:13:47 -0600

    cambio de saludo

--- interest_rate.py
+++ interest_rate.py
@@ -1,13 +1,1 @@
-# *****************
-# INTERÉS COMPUESTO
-# *****************
-
-
-def run(amount: float, rate: float, years: int) -> float:
-    # TU CÓDIGO AQUÍ
-    future_amount = amount * (1 + rate / 100) ** years
-    return future_amount  # Retorna el monto futuro calculado
-
-
-if __name__ == '__main__':
-    run(10000, 3.5, 7)
+print(HelloWorld)
