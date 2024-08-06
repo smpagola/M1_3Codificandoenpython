7d043beb8ce51d3885b2634aefa1a2cc09208dae
smpagola <pagolincom@gmail.com>
2024-08-06 13:07:23 -0600

    initial commit

---
+++ triangle_area.py
@@ -0,0 +1,13 @@
+# ********************
+# ÁREA DE UN TRIÁNGULO
+# ********************
+
+
+def run(base: float, height: float) -> float:
+    # TU CÓDIGO AQUÍ
+    area = 0.5 * base * height
+    return area
+
+
+if __name__ == '__main__':
+    run(3, 3)
